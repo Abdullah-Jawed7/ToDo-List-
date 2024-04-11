58d934ff30074955436dd5ffbd314f89135ee14b
One-andOnly <162917702+One-andOnly@users.noreply.github.com>
2024-04-11 18:58:07 +0500

    Manage our daily tasks here

--- README.md
+++ README.md
@@ -1,1 +1,37 @@
-# ToDo-List-
+ # To-Do List Manager 📝
+
+Welcome to the To-Do List Manager! Keep track of your tasks efficiently with this command-line tool.
+
+# Installation
+
+You can install the To-Do List Manager via npm:
+
+    npm i @oneandonly/todo-list
+
+## Usage
+To start managing your to-do list, run the following command in your terminal:
+
+    npx @oneandonly/todo-list
+
+Follow the on-screen prompts to add, view, or remove tasks from your list.
+
+## Features
+- Add tasks to your to-do list
+- View the current list of tasks
+- Remove tasks from the list
+
+## Contributing
+Contributions are welcome! If you have any suggestions, bug reports, or want to contribute new features, feel free to open an issue or submit a pull request.
+
+## License
+This project is licensed under the MIT License.
+
+## Author
+Abdullah Jawed
+
+## Connect
+My GitHub: [Click me](https://github.com/One-andOnly?tab=repositories)
+
+My LinkedIn: [Click me](https://www.linkedin.com/in/abdullah-muhammad-jawed-60b27a282)
+
+# Enjoy managing your tasks! 🚀
---
+++ list.js
@@ -0,0 +1,66 @@
+#!  /usr/bin/env node
+import inquirer from "inquirer";
+let todos = [];
+let condition = true;
+//  Function for removing a task
+let removeTask = async () => {
+    await viewTasks();
+    let deleteItem = await inquirer.prompt({
+        name: "remove",
+        type: "input",
+        message: `Enter the number of the task you want delete:`,
+    });
+    // Deleting the selected item from array
+    let deletedtask = todos.splice(deleteItem.remove - 1, 1);
+    console.log(`\n ${deletedtask} is successfully deleted from your todos `);
+};
+do {
+    const addtask = await inquirer.prompt([
+        {
+            name: "todo",
+            type: "list",
+            message: "What you want to perform with your Todos ?",
+            choices: ["Add Task", "Delete Task", "View All Tasks", "Exit"],
+        },
+    ]);
+    if (addtask.todo == "Add Task") {
+        const task = await inquirer.prompt([
+            {
+                name: "yourTask",
+                type: "input",
+                message: "Enter your Task ",
+            },
+        ]);
+        todos.push(task.yourTask);
+        console.log(todos);
+    }
+    else if (addtask.todo === "View All Tasks") {
+        //console.log("Your Tasks are :" + todos);
+        await viewTasks();
+    }
+    else if (addtask.todo == "Delete Task") {
+        await removeTask();
+    }
+    else {
+        condition = false;
+    }
+} while (condition);
+// Function for view task
+function viewTasks() {
+    console.log("\n Your tasks are as follows:\n");
+    todos.forEach((task, index) => {
+        console.log(`${index + 1}- ${task}`);
+    });
+}
+/*  Function for removing a task
+let removeTask = async () => {
+  await viewTasks();
+  let deleteItem = await inquirer.prompt({
+    name: "remove",
+    type: "input",
+    message: `Enter the number of the task you want delete:`,
+  });
+  // Deleting the selected item from array
+  let deletedtask = todos.slice(deleteItem.remove - 1);
+  console.log(`\n ${deletedtask} is successfully deleted from your todos `);
+};*/
